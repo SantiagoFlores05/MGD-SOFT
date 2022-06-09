785c15826396c8980ccb7ad461ec424669982044
AudinMonti12 <100879367+AudinMonti12@users.noreply.github.com>
2022-06-09 07:49:01 -0500

    Add files via upload

---
+++ Avance proyecto 26-5-2022/MVC MGD SOFT/MGD SOFT/core/router.php
@@ -0,0 +1,30 @@
+<?php
+
+    function cargarControlador($controlador){
+        $nombreControlador=ucwords( $controlador)."controller";
+        $archivoControlador ='controllers/'.ucwords($controlador).'.php';
+
+        if(!is_file($archivoControlador)){
+            $archivoControlador='controllers/'.CONTROLADOR_PRINCIPAL.'.php';
+
+        }
+       // echo $archivoControlador;
+
+        require_once $archivoControlador;
+        $control= new $nombreControlador();
+        return $control;
+        
+    }
+
+    function cargarAccion($controller,$accion){
+        if(isset($accion) && method_exists($controller,$accion)){
+            $controller->$accion();
+        }else{
+            $controller->ACCION_PRINCIPAL();
+        }
+    }
+
+
+
+
+?>}
